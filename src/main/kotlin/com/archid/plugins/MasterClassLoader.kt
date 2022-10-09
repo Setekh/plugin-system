9c956dbb0bb94a17149d55795c17a937e5ee7544
Vlad Ravenholm <vlad.ravenholm@gmail.com>
2022-10-09 10:23:29 +0300

    Trigger a build

--- src/main/kotlin/com/archid/plugins/MasterClassLoader.kt
+++ src/main/kotlin/com/archid/plugins/MasterClassLoader.kt
@@ -5,9 +5,6 @@ import java.net.URLClassLoader
 
 
 open class MasterClassLoader(urls: List<URL>, parent: ClassLoader? = null) : URLClassLoader(urls.toTypedArray<URL>(), parent) {
-
-    fun addUrl(url: URL) = super.addURL(url)
-
     fun addUrls(urls: List<URL?>) {
         for (u in urls) {
             addURL(u)
